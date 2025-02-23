650aab2d4d09f0b2660426a693c3df3aee6f2413
Artemkuz <artemkuz1083@gmail.com>
2025-02-23 22:13:34 +0500

    DB

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,12 +1,20 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
 import java.util.Date;
 
-public class Post {
+@Entity
+public class Post implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long id;
     private String text;
     private Integer likes = 0;
     private Date creationDate;
-    private Long id;
 
     public Post(Long id, String text, Date date) {
         this.id = id;
@@ -14,6 +22,10 @@ public class Post {
         this.creationDate = date;
 
     }
+
+    public Post() {
+    }
+
 
     public String getText() {
         return text;
